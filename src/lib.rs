38be5dcc407a935c85aaecec45efaca0232e79d9
danielteberian <danielteberian@github.com>
2021-10-09 13:04:35 -0500

    This crate compiles without error, so it is being released.

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+extern crate alloc;
+use alloc::boxed::Box;
+
+pub struct GoodnightCallback(Option<Box<dyn FnOnce()>>);
+impl GoodnightCallback
+{
+	pub fn new(callback: Box<dyn FnOnce()>) -> Self
+	{
+		Self(Some(callback))
+	}
+}
+
+impl Drop for GoodnightCallback
+{
+	fn drop(&mut self)
+	{
+		(self.0.take().unwrap())();
+	}
+}
+#[macro_export]
+macro_rules! goodnight
+{
+	($closure:ident) => {
+		let _goodnight_callback_whatever = $crate::GoodnightCallback::new(Bow::new($closure));
+	};
+	(move || $callback:expr) => {
+		let close = move || $callback;
+		goodnight!(close);
+	};
+	(|| $callback:expr) => {
+		let close = || $callback;
+		goodnight!(close);
+	};
+	($callback:expr) => {
+		let close = || $callback;
+		goodnight!(close);
+	};
+	($callback:block) =>
+	{
+		let close = || $cb;
+		goodnight!(close);
+	};
+}
